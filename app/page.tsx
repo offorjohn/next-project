b5a80f5ca3887c19ecb9c0ac2c18e7cc37f8f7a6
offor <offorchukwuebuka11@gmail.com>
2025-03-29 13:18:00 +0100

    work ongoing

--- app/page.tsx
+++ app/page.tsx
@@ -86,7 +86,7 @@ export default function Home() {
 
             <div>
               <button
-                className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 style={{ backgroundColor: "#415a77" }}
               >
                 Log in
